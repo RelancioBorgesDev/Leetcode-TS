759a3a6775a174e85c9c1dc47490b2db55f8c13a
RelancioBorgesDev <relancio.dev@gmail.com>
2024-02-09 09:48:20 -0300

    validAnagram

---
+++ strings/validAnagram.ts
@@ -0,0 +1,38 @@
+function isAnagram(s: string, t: string): boolean {
+  let mapS = new Map<string, number>();
+  let mapT = new Map<string, number>();
+
+  if (s.length != t.length) {
+    return false;
+  }
+
+  for (let char of s) {
+    if (mapS.has(char)) {
+      mapS.set(char, mapS.get(char)! + 1);
+    } else {
+      mapS.set(char, 1);
+    }
+  }
+
+  for (let char of t) {
+    if (mapT.has(char)) {
+      mapT.set(char, mapT.get(char)! + 1);
+    } else {
+      mapT.set(char, 1);
+    }
+  }
+
+  for (let char of s) {
+    if (mapS.get(char) != mapT.get(char)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+//85ms
+
+console.log(isAnagram("anagram", "nagaram"));
+console.log(isAnagram("rat", "car"));
+console.log(isAnagram("ab", "a"));
