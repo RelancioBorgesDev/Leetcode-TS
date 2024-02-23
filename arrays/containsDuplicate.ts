07e39bbdcb6ef51dbe4bbe12539b2d2b1b448210
RelancioBorgesDev <relancio.dev@gmail.com>
2024-02-23 11:49:34 -0300

    containsDuplicate - review

--- arrays/containsDuplicate.ts
+++ arrays/containsDuplicate.ts
@@ -1,16 +1,5 @@
 function containsDuplicate(nums: number[]): boolean {
-  const seen = new Set<number>();
-
-  for (const num of nums) {
-    if (seen.has(num)) {
-      return true;
-    }
-    seen.add(num);
-  }
-
-  return false;
+  return nums.length == new Set(nums).size ? false : true;
 }
 
 console.log(containsDuplicate([1, 2, 3, 1]));
-
-
