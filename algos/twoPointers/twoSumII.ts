3e5df5be929e98f7f51ccb47d73a5f6ce74f9344
RelancioBorgesDev <relancio.dev@gmail.com>
2024-02-26 11:21:38 -0300

    twoSumII (Optimal Sol)

--- algos/twoPointers/twoSumII.ts
+++ algos/twoPointers/twoSumII.ts
@@ -20,3 +20,26 @@ function twoSum(numbers: number[], target: number): number[] {
   return [];
 }
 //1000 ms solution
+
+
+
+//52 ms sol
+function twoSumOptimal(numbers: number[], target: number): number[] {
+    let size = numbers.length;
+    let p1 = 0;
+    let p2 = size - 1;
+    
+    while(p1 < p2){
+        let sum = numbers[p1] + numbers[p2];
+
+        if(sum == target){
+            return [p1+1, p2+1]
+        }else if(sum > target) {
+            p2--;
+        }else if(sum < target){
+            p1++;
+        }
+    }
+
+    return [];
+};
